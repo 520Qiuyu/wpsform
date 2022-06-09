09b521e2c439f482b144715035ef25addeff13f0
qiuyu <1943684871@qq.com>
2022-06-09 22:14:41 +0800

    小改动

--- wpsform/src/views/FormList.vue
+++ wpsform/src/views/FormList.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="form-list">
-<<<<<<< HEAD
     <div class="form-list-aside" :style="{ height: adideHeight }">
       <div class="form-list-aside-top">
         <router-link to="/app/new-form-create" class="form-create-btn"
           >新建表单</router-link
         >
-=======
-    <div class="form-list-aside" :style="{height:adideHeight}">
-      <div class="form-list-aside-top">
-        <router-link to="/app/new-form-create" class="form-create-btn">新建表单</router-link>
->>>>>>> 6368583d5d3d79f011c967cf924935296a2c3a23
       </div>
       <span class="form-list-title">表单列表</span>
     </div>
     <div class="form-list-container">
-<<<<<<< HEAD
       <router-link to="/app/new-form-result">
         <p class="form-list-item">点击进入表单1详情</p>
       </router-link>
@@ -28,34 +21,20 @@
       <router-link to="/app/new-form-result">
         <p class="form-list-item">点击进入表单4详情</p>
       </router-link>
-=======
-      <p>表单1</p>
-      <p>表单2</p>
->>>>>>> 6368583d5d3d79f011c967cf924935296a2c3a23
     </div>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import { defineComponent, ref } from "vue";
-=======
-import { defineComponent,ref } from "vue";
->>>>>>> 6368583d5d3d79f011c967cf924935296a2c3a23
 export default defineComponent({
   name: "FormList",
   components: {},
   props: {},
   setup(props, ctx) {
-<<<<<<< HEAD
     const adideHeight = ref(window.innerHeight - 115 + "px");
     return {
       adideHeight,
-=======
-    const adideHeight = ref(window.innerHeight - 115 + 'px')
-    return {
-      adideHeight
->>>>>>> 6368583d5d3d79f011c967cf924935296a2c3a23
     };
   },
 });
@@ -98,7 +77,6 @@ export default defineComponent({
   margin: 20px;
   margin-bottom: 100px;
   border: 1px solid #ccc;
-<<<<<<< HEAD
   padding: 30px;
 }
 
@@ -109,8 +87,4 @@ export default defineComponent({
   margin-bottom: 10px;
   padding: 0 20px;
 }
-=======
-}
-
->>>>>>> 6368583d5d3d79f011c967cf924935296a2c3a23
 </style>
