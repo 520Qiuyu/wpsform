4adc150023567815198c2f4cb0250801ab099c2e
zhuyongfan <2607559902@qq.com>
2022-06-11 11:59:06 +0800

    增加devServer

--- wpsform/vue.config.js
+++ wpsform/vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
