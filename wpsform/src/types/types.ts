0b45609340a90265904b0e842216a5bde6b26201
黄天宇 <1504889174@qq.com>
2022-06-15 12:39:51 +0800

    添加获取表单详情api和表单详情接口

--- wpsform/src/types/types.ts
+++ wpsform/src/types/types.ts
@@ -8,35 +8,56 @@ export interface IUser {
 
 export interface IForm {
   id: string;           //表单ID
-  offset?: number;      //
-  limit?: number;       //
+  offset?: number;      //获取表单列表时的起始索引，默认为0
+  limit?: number;       //获取表单列表时的表单数量
   isStar?: boolean;     //表单是否标星
   title: string;        //表单标题
   subTitle: string;     //表单副标题
   ctime: number;        //表单创建时间
   status: number;       //表单状态，2为草稿，3为收集中，4为已结束 
-  problems: {           //表单问题
-    title: string;
-    type:
-      | "input"
-      | "singleSelect"
-      | "multiSelect"
-      | "pullSelect"
-      | "date"
-      | "time"
-      | "score";
-    required: boolean;
-    setting?: {
-      options: {
-        title: string;
-        status: 1 | 2;
-      }[];
-    };
-  }[];
+  problems: IProblem[];
+  // problems: {           
+  //   title: string;
+  //   type:
+  //     | "input"
+  //     | "singleSelect"
+  //     | "multiSelect"
+  //     | "pullSelect"
+  //     | "date"
+  //     | "time"
+  //     | "score";
+  //   required: boolean;
+  //   setting?: {
+  //     options: {
+  //       title: string;
+  //       status: 1 | 2;
+  //     }[];
+  //   };
+  //   result?: {
+  //     value:
+  //       | string
+  //       | number
+  //       | {
+  //           id: string
+  //           title: string
+  //         }
+  //       | {
+  //           id: string
+  //           title: string
+  //         }[]
+  //   }
+  // }[];
+}
+
+export interface IFormResult {
+  id: string;           //表单详情id
+  formAuthor: string;   //填写人
+  formId: string;       //表单id
+  result: IProblem[];   //填写结果
 }
 
 export interface IProblem {
-  id?: string;
+  id: string;
   title: string;
   type:
     | "input"
@@ -54,6 +75,19 @@ export interface IProblem {
       status: 1 | 2;
     }[];
   };
+  result?: {
+    value:
+      | string
+      | number
+      | {
+          id: string
+          title: string
+        }
+      | {
+          id: string
+          title: string
+        }[]
+  }
   [propName: string]: any;
 }
 
